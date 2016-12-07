0eb4d4000a5b01f095059837eb951508c49b364b
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-07 23:39:49 +0800

    added methods

--- hangman/lib/hangman/game.rb
+++ hangman/lib/hangman/game.rb
@@ -7,12 +7,30 @@ module Hangman
 			@guessed_letters  = []
 		end
 
+		def guess
+			while true
+
+				if out_of_turns?
+
+				end
+			mistakes_left -= 1
+			end
+		end
+
+		def game_over
+			return :out if out_of_turns?
+		end
+
 		def out_of_turns?
 			if mistakes_left == 0
 				true
 			else
 				false
 			end
+		end
+
+		def game_over_message
+			return "Out of turns!" if game_over == :out
 		end
 
 

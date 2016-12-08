53c1b0c9885003c79eaad52da1523cbfcdfc05b5
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-08 14:04:50 +0800

    added methods and tests

--- hangman/lib/hangman/game.rb
+++ hangman/lib/hangman/game.rb
@@ -1,24 +1,65 @@
 module Hangman
 	class Game
-		attr_accessor :mistakes_left, :secret_word, :guessed_letters
-		def initialize
+		attr_accessor :mistakes_left, :secret_word, :guessed_letters, :current_word
+		def initialize(word_to_use = Dictionary.new.word)
 			@mistakes_left = 5
-			@secret_word = Dictionary.new
+			@secret_word = word_to_use.split("")
 			@guessed_letters  = []
 		end
 
-		def guess
+		def start
+
 			while true
 
 				if out_of_turns?
-
+					game_over_message
 				end
 			mistakes_left -= 1
 			end
 		end
 
-		def game_over
-			return :out if out_of_turns?
+		def ask_input
+			puts "Enter a letter"
+			input = gets.chomp
+		end
+
+		def update_current_word(letter)
+			temp = []
+			#secret_word = secret_word.split("")
+			secret_word.each_with_index do |x, indx|
+				if x == letter
+					temp.push(indx)
+				end
+			end
+
+			temp.each do |x|
+				current_word.delete_at(x)
+				current_word.insert(x,letter)
+			end
+
+			current_word
+		end
+
+		def create_board
+			@current_word = Array.new(secret_word.length, "_")
+		end
+
+		def display_current_word
+			current_word.each do |x|
+				print "#{x} "
+			end
+		end
+
+		def check_victory
+			return :win if victory?
+			return :out if out_of_turns?s
+		end
+
+		def victory?
+			p current_word
+			puts
+			p secret_word
+			return true if current_word.join == secret_word.join
 		end
 
 		def out_of_turns?
--- hangman/spec/game_spec.rb
+++ hangman/spec/game_spec.rb
@@ -10,19 +10,64 @@ module Hangman
 
 			it "initializes with a secret word" do
 				g = Game.new
-				expect(g.secret_word.word).to have_at_least(1).thing
+				expect(g.secret_word).to have_at_least(1).thing
 			end
 		end
 
-		context "#guess" do
+		context "#start" do
 
 		end
 
-		context "#game_over" do
-			it "returns :out if out_of_turns returns true" do
+		context "#create_board" do
+			it "returns an array of underscores" do
 				g = Game.new
+				arr = Array.new(g.secret_word.length, "_")
+				expect(g.create_board).to eq arr
+			end
+		end
+
+		context "#display_current_word" do
+			it "returns a bunch of underscores by default" do
+				g = Game.new
+				g.create_board
+				arr = Array.new(g.secret_word.length,"_")
+				expect(g.display_current_word).to eq arr
+			end
+		end
+
+		context "#update_current_word" do
+			it "returns a new array that includes a letter passed" do
+				g = Game.new("flagger")
+				g.create_board
+				#expect(g.update_current_word("g")).to eq ["_"]
+				g.update_current_word("g")
+				expect(g.current_word.join).to eq "___gg__"
+				g.update_current_word("r")
+				expect(g.current_word.join).to eq "___gg_r"
+			end
+		end
+
+		context "#check_victory" do
+			it "returns :out if out_of_turns? returns true" do
+				g = Game.new
+				g.current_word = ["a","b"]
 				allow(g).to receive(:out_of_turns?).and_return(true)
-				expect(g.game_over).to eq :out
+				expect(g.check_victory).to eq :out
+			end
+
+			it "returns :win if victory? returns true" do
+				g = Game.new
+				#g.current_word = ["a","b"]
+				allow(g).to receive(:victory?).and_return(true)
+				expect(g.check_victory).to eq :win
+			end
+		end
+
+		context "#victory?" do
+			it "returns true if current.word is equal to secret.word" do
+				g = Game.new("beaner")
+				g.current_word = ["b","e","a","n","e","r"]
+				expect(g.victory?).to be true
 			end
 		end
 
